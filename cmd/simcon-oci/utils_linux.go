2049674f8991887fb810bb7c4be0452913bcdac9
yoonhyunwoo <yjs88zerg@gmail.com>
2025-03-19 12:22:22 +0900

    isolate init process for create

--- cmd/simcon-oci/utils_linux.go
+++ cmd/simcon-oci/utils_linux.go
@@ -65,23 +65,13 @@ func createContainer(context *cli.Context) (int, error) {
 
 	// Networks 설정
 
-	// 컨테이너 생성 (자식 프로세스)
-	pid, err := forkContainerProcess(config, containerDir)
-	if err != nil {
-		return -1, err
-	}
-
-	// PID 저장
-	pidFile := fmt.Sprintf("%s/pidfile", containerDir)
-	if err := os.WriteFile(pidFile, []byte(fmt.Sprintf("%d", pid)), 0644); err != nil {
-		return -1, err
-	}
-
-	return pid, nil
 }
 
-func forkContainerProcess(config *specs.Spec, containerDir string) (int, error) {
-	// simcon-oci process를 복제
+func initContainerProcess(config *specs.Spec, containerDir string) (int, error) {
+	runtime.GOMAXPROCS(1)
+	// 쓰레드 잠금
+	runtime.LockOSThread()
+
 	cmd := exec.Command("/proc/self/exe", "init", containerDir)
 
 	// main process stdio setup
@@ -118,28 +108,4 @@ func forkContainerProcess(config *specs.Spec, containerDir string) (int, error) 
 	}
 
 	return pid, nil
-}
-
-func containerInitProcess(context *cli.Context) error {
-	runtime.GOMAXPROCS(1)
-	// 쓰레드 잠금
-	runtime.LockOSThread()
-
-	containerDir := context.Args().First()
-
-	if containerDir == "" {
-		return fmt.Errorf("containerDir Args require")
-	}
-
-	// 1. rootfs 마운트 준비
-	// 2. pivot_root 실행
-	// 3. /proc, /sys 마운트 등 환경 준비
-	// 4. exec로 실제 컨테이너 프로세스 실행
-
-	if err != nil {
-		fmt.Printf("init 실패: %v\n", err)
-		return err
-	}
-
-	return nil
 }
