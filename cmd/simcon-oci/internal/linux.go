ee83c947afef30d8825117933830623a7bfd27e0
YoonHyunWoo <yjs88zerg@gmail.com>
2025-03-27 22:47:26 +0900

    add support spec.linux

--- cmd/simcon-oci/internal/linux.go
+++ cmd/simcon-oci/internal/linux.go
@@ -24,23 +24,137 @@ func readonlyPath(path string) error {
 	return nil
 }
 
-// TODO : Process resources
-func resources(spec *specs.Spec) error {
+func setResources(spec *specs.Spec) error {
+	if spec.Linux == nil || spec.Linux.Resources == nil {
+		return nil
+	}
+
 	cgroupPath := spec.Linux.CgroupsPath
-	// set device
-	devicefile := filepath.Join(cgroupPath, "cgroup.devices.allow")
-	devicesCgroup, err := os.Create(devicefile)
+	res := spec.Linux.Resources
+
+	// Memory
+	if res.Memory != nil {
+		if res.Memory.Limit != nil {
+			err := writeFile(filepath.Join(cgroupPath, "memory.max"), fmt.Sprintf("%d", *res.Memory.Limit))
+			if err != nil {
+				return err
+			}
+		}
+		if res.Memory.Swap != nil {
+			err := writeFile(filepath.Join(cgroupPath, "memory.swap.max"), fmt.Sprintf("%d", *res.Memory.Swap))
+			if err != nil {
+				return err
+			}
+		}
+		if res.Memory.Swappiness != nil {
+			err := writeFile(filepath.Join(cgroupPath, "memory.swappiness"), fmt.Sprintf("%d", *res.Memory.Swappiness))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// CPU
+	if res.CPU != nil {
+		if res.CPU.Quota != nil && res.CPU.Period != nil {
+			value := fmt.Sprintf("%d %d", *res.CPU.Quota, *res.CPU.Period)
+			err := writeFile(filepath.Join(cgroupPath, "cpu.max"), value)
+			if err != nil {
+				return err
+			}
+		}
+		if res.CPU.Shares != nil {
+			err := writeFile(filepath.Join(cgroupPath, "cpu.weight"), fmt.Sprintf("%d", *res.CPU.Shares))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// PIDs
+	if res.Pids != nil {
+		err := writeFile(filepath.Join(cgroupPath, "pids.max"), fmt.Sprintf("%d", res.Pids.Limit))
+		if err != nil {
+			return err
+		}
+	}
+
+	// Block IO
+	if res.BlockIO != nil {
+		if res.BlockIO.Weight != nil {
+			err := writeFile(filepath.Join(cgroupPath, "io.weight"), fmt.Sprintf("%d", *res.BlockIO.Weight))
+			if err != nil {
+				return err
+			}
+		}
+		// 생략: device 별 블록 IO 제한 처리 (추가 구현 가능)
+	}
+
+	// Hugepages
+	for _, hp := range res.HugepageLimits {
+		file := fmt.Sprintf("hugetlb.%s.max", hp.Pagesize)
+		err := writeFile(filepath.Join(cgroupPath, file), fmt.Sprintf("%d", hp.Limit))
+		if err != nil {
+			return err
+		}
+	}
+
+	// Network
+	if res.Network != nil {
+		if res.Network.ClassID != nil {
+			err := writeFile(filepath.Join(cgroupPath, "net_cls.classid"), fmt.Sprintf("%d", *res.Network.ClassID))
+			if err != nil {
+				return err
+			}
+		}
+		for _, prio := range res.Network.Priorities {
+			line := fmt.Sprintf("%s %d", prio.Name, prio.Priority)
+			err := writeFile(filepath.Join(cgroupPath, "net_prio.ifpriomap"), line)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// RDMA
+	if res.Rdma != nil {
+		for dev, limit := range res.Rdma {
+			if limit.HcaHandles != nil {
+				err := writeFile(filepath.Join(cgroupPath, "rdma."+dev+".hca_handle.max"), fmt.Sprintf("%d", *limit.HcaHandles))
+				if err != nil {
+					return err
+				}
+			}
+			if limit.HcaObjects != nil {
+				err := writeFile(filepath.Join(cgroupPath, "rdma."+dev+".hca_object.max"), fmt.Sprintf("%d", *limit.HcaObjects))
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// Unified
+	for key, value := range res.Unified {
+		err := writeFile(filepath.Join(cgroupPath, key), value)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Devices
+	devPath := filepath.Join(cgroupPath, "cgroup.devices.allow")
+	f, err := os.OpenFile(devPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return err
 	}
-	defer devicesCgroup.Close()
-
-	for _, resource := range spec.Linux.Resources.Devices {
-		if !resource.Allow {
+	defer f.Close()
+	for _, dev := range res.Devices {
+		if !dev.Allow {
 			continue
 		}
-		row := fmt.Sprintf("%s %d:%d %s", resource.Type, resource.Major, resource.Minor, resource.Access)
-		_, err = devicesCgroup.WriteString(row)
+		row := fmt.Sprintf("%s %d:%d %s\n", dev.Type, *dev.Major, *dev.Minor, dev.Access)
+		_, err := f.WriteString(row)
 		if err != nil {
 			return err
 		}
@@ -72,4 +186,9 @@ func setNamespaces(namespaces []specs.LinuxNamespace) uintptr {
 		}
 	}
 	return cloneFlags
+}
+
+// writeFile 유틸 함수
+func writeFile(path, content string) error {
+	return os.WriteFile(path, []byte(content), 0644)
 }
